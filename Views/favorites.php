3cd2e314c9fe5347ba85525cc8e294dddefb4932
Anita Mjeshtri <122668935+AnitaMjeshtri@users.noreply.github.com>
2023-06-23 19:03:53 +0200

    editing 'suggested for you'

--- Views/favorites.php
+++ Views/favorites.php
@@ -14,8 +14,9 @@ require_once './navbar.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel = "stylesheet" href="../assets/css/favorite.css">
   <link rel = "stylesheet"href="../assets/css/carouselCss.css">
+  <!-- <script src="https://kit.fontawesome.com/9cfc78147e.js" crossorigin="anonymous"></script> -->
+  <script src="https://kit.fontawesome.com/872dda741a.js" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://kit.fontawesome.com/9cfc78147e.js" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
   rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
   crossorigin="anonymous">
@@ -196,8 +197,7 @@ require_once './navbar.php';
     <button class="third"></button>
   </div>
 </div> -->
-
 <?php include "carousel.html" ?>
-<!-- <?php include "footer.html"?> -->
+<?php include "footer.html"?>
   </body>
 </html>
