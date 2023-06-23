2123c449d1d50156137359ed1adc806ae7ae1d68
Anita Mjeshtri <122668935+AnitaMjeshtri@users.noreply.github.com>
2023-06-23 12:19:08 +0200

    carousel added (favorites, ordered)

--- Views/favorites.php
+++ Views/favorites.php
@@ -13,6 +13,7 @@ require_once './navbar.php';
   <meta http-equiv = "X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel = "stylesheet" href="../assets/css/favorite.css">
+  <link rel = "stylesheet"href="../assets/css/carouselCss.css">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://kit.fontawesome.com/9cfc78147e.js" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
@@ -147,6 +148,56 @@ require_once './navbar.php';
 </script>
   </div>
 </div>
-<?php include "footer.html"?>
+
+<!-- <div class="center">
+  <div class="wrapper">
+    <div class="inner">
+      <div class="cardd">
+      
+      </div>
+      <div class="cardd">
+        <img src="../assets/images/broken.jpeg">
+    
+      </div>
+      <div class="cardd">
+        <img src="../assets/images/lord.jpg">
+      
+      </div>
+      <div class="cardd">
+        <img src="../assets/images/poter.jpg">
+    
+      </div>
+      <div class="cardd">
+        <img src="../assets/images/thief.jpg">
+    
+      </div>
+      <div class="cardd">
+        <img src="../assets/images/thief.jpg">
+      
+      </div>
+            <div class="cardd">
+        <img src="../assets/images/logo.png">
+    
+      </div>
+            <div class="cardd">
+        <img src="../assets/images/lord.jpg">
+      
+      </div>
+            <div class="cardd">
+        <img src="../assets/images/poter.jpg">
+      
+      </div>
+    </div>
+  </div>
+  <script src="../Controller/carouselCon.js"> </script>
+  <div class="map">
+    <button class="active first"></button>
+    <button class="second"></button>
+    <button class="third"></button>
+  </div>
+</div> -->
+
+<?php include "carousel.html" ?>
+<!-- <?php include "footer.html"?> -->
   </body>
 </html>
