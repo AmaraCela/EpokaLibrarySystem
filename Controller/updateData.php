a2427966b56e9c31bd38e45f3c26d7c5edc168b8
Anita Mjeshtri <122668935+AnitaMjeshtri@users.noreply.github.com>
2023-06-23 22:53:31 +0200

    profile editing option added

--- Controller/updateData.php
+++ Controller/updateData.php
@@ -1,38 +1,76 @@
-<script>$(document).on('click', '.edit', function() {
-  // document.getElementById("name").disabled=false;
-  $(this).parent().siblings('td.data').each(function() {
-    var content = $(this).html();
-    var std = $(this).html('<input value="' + content + '" />');
-    console.log(std);
-  });
-  $(this).siblings('.save').show();
-});
-$(document).on('click', '.save', function() {
-  $('input').each(function() {
-    var content = $(this).val();
-    $(this).html(content);
-    $(this).contents().unwrap();
-  });  
-  $(this).siblings('.edit').show();
-  
-});
-$('.add').click(function() {
-  $(this).parents('table').append('<tr><td class="data"></td><td class="data"></td><td class="data"></td><td><button class="save">Save</button><button class="edit">Edit</button> <button class="delete">Delete</button></td></tr>');  
-});</script>
-<?php if(isset($_POST['saveName'])){
-  $id = $_SESSION['id'];
-  // $sql = "SELECT `Name` FROM `student` WHERE id=$id";
-  // $res = mysqli_query($db,$sql);
-  // $name = mysqli_fetch_assoc($res);
+<?php include "../Model/Connection.php";
+if(isset($_GET['did'])){
+  $id = $_GET['did'];
+  $sql = "SELECT * FROM `student` WHERE id=$id";
+  $res = mysqli_query($db,$sql);
+  if($res){
+    $row=mysqli_fetch_assoc($res);
+    $name = $row['Name'];
+    $surname = $row['Surname'];
+    $email = $row['Email'];
+    $mobile = $row['PhoneNumber'];
+    $password = $row['Password'];
+  } 
 
-  $name = $_POST['editName'];
-  $sql = "UPDATE `Name` set `Name`='$name' WHERE id=$id";
-  $result = mysqli_query($db,$sql);
-  if($result){
-    header('location: ../Views/profile.php');
+  if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    $sql = "UPDATE `student` SET `Name`='$name', `Surname`='$surname', `Email`='$email',`PhoneNumber`='$mobile',`Password`='$password'";
+    $result = mysqli_query($db, $sql);
+    if($result){
+      header('location: ../Views/profile.php');
+    }else{
+      die(mysqli_error($db));
+    }
   }
-} ?>
-<!-- <script>
-  let isName = document.getElementsByClassName('saveName');
-  let isSurname = document.getElementsByClassName('saveSurname');
-</script> -->
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Edit Profile</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container my-5">
+    <form method="post">
+      <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" name="name" placeholder="Enter your name" autocomplete="off" value="<?= $name ?>">
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Surname</label>
+        <input type="text" class="form-control" name="surname" placeholder="Enter your surname" autocomplete="off" value="<?= $surname ?>">
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" class="form-control" name="email" placeholder="Enter your email" autocomplete="off" value="<?= $email ?>">
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Mobile</label>
+        <input type="text" class="form-control" name="mobile" placeholder="Enter your mobile" autocomplete="off" value="<?= $mobile ?>">
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" class="form-control" name="password" placeholder="Enter your password" autocomplete="off" value="<?= $password ?>">
+      </div>
+
+      <button type="submit" class="btn btn-primary" name="submit">Update</button>
+    </form>
+  </div>
+</body>
+
+</html>
